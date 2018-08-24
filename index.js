8b88dc9307c28e5154d551b44b9d9b0371d83903
Hayley <hayley.d.prior@gmail.com>
2018-08-24 15:44:45 +0100

    add test example

--- index.js
+++ index.js
@@ -78,37 +78,37 @@
 //   })
 //   .attr('fill', '#000')
 
-// // scale
-// var dataset = [1, 2, 3, 4, 5]
-//
-// var svgWidth = 500
-// var svgHeight = 300
-// var barPadding = 5
-// var barWidth = (svgWidth / dataset.length)
-//
-// var svg = d3.select('svg')
-//   .attr('width', svgWidth)
-//   .attr('height', svgHeight)
-//
-// var yScale = d3.scaleLinear()
-//   .domain([0, d3.max(dataset)])
-//   .range([0, svgHeight])
-//
-// var barChart = svg.selectAll('rect')
-//   .data(dataset)
-//   .enter()
-//   .append('rect')
-//   .attr('y', function (d) {
-//     return svgHeight - yScale(d)
-//   })
-//   .attr('height', function (d) {
-//     return yScale(d)
-//   })
-//   .attr('width', barWidth - barPadding)
-//   .attr('transform', function (d, i) {
-//     var translate = [barWidth * i, 0]
-//     return 'translate(' + translate + ')'
-//   })
+// scale
+var dataset = [1, 2, 3, 4, 5]
+
+var svgWidth = 500
+var svgHeight = 300
+var barPadding = 5
+var barWidth = (svgWidth / dataset.length)
+
+var svg = d3.select('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgHeight])
+
+var barChart = svg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+  .attr('y', function (d) {
+    return svgHeight - yScale(d)
+  })
+  .attr('height', function (d) {
+    return yScale(d)
+  })
+  .attr('width', barWidth - barPadding)
+  .attr('transform', function (d, i) {
+    var translate = [barWidth * i, 0]
+    return 'translate(' + translate + ')'
+  })
 
 // // Axes
 // // d3.axisTop()
---
+++ test.js
@@ -0,0 +1,58 @@
+var dataset = [
+  { 'name': 'Total Income', 'value': 1908.00 },
+  { 'name': 'Total Spending', 'value': 3433.00 },
+  { 'name': 'Overspend', 'value': -1525.00 }
+]
+
+var sortedData = dataset.sort(function (a, b) {
+  return b.value - a.value
+})
+var maxValue = sortedData[0].value
+var minValue = sortedData[sortedData.length - 1].value
+
+var margin = { top: 20, right: 30, bottom: 40, left: 30 }
+var width = 960 - margin.left - margin.right
+var height = 500 - margin.top - margin.bottom
+
+var barPadding = 5
+var barWidth = (height / dataset.length)
+
+var xScale = d3.scaleLinear()
+  .domain([minValue, maxValue])
+  .range([0, width])
+
+var yScale = d3.scaleBand()
+  .range([0, height])
+
+var xAxis = d3.axisBottom()
+  .scale(xScale)
+
+var yAxis = d3.axisLeft()
+  .scale(yScale)
+
+var svg = d3.select('#averages_bar_chart')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+svg.selectAll('.bar')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('class', function (d) { return 'bar bar--' + (d.value < 0 ? 'negative' : 'positive') })
+    .attr('x', function (d) { return xScale(Math.min(0, d.value)) })
+    .attr('y', function (d, i) { return height - i * barWidth })
+    .attr('width', function (d) { return Math.abs(xScale(d.value) - xScale(0)) })
+    .attr('height', height)
+
+svg.append('g')
+  .attr('class', 'x axis')
+  .attr('transform', 'translate(0,' + height + ')')
+  .call(xAxis)
+
+svg.append('g')
+  .attr('class', 'y axis')
+  .attr('transform', 'translate(' + xScale(0) + ',0)')
+  .call(yAxis)
